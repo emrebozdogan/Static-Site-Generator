674b2f75754c248c0acaeaf633399747acd5eaec
emrebozdogan <emrbzdgn@icloud.com>
2025-08-22 14:46:22 +0300

    Update gencontent.py - remove comments and clean up recursive directory processing

--- src/gencontent.py
+++ src/gencontent.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
     raise ValueError("No title found")
         
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as markdown_file:
@@ -25,14 +25,16 @@ def generate_page(from_path, template_path, dest_path):
     extracted_title = extract_title(content)
 
     final_html = template.replace("{{ Title }}", extracted_title).replace("{{ Content }}", html_content)
-
+    final_html = final_html.replace('href="/', f'href="{basepath}')
+    final_html = final_html.replace('src="/', f'src="{basepath}')
+    
     import os
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as dest_file:
         dest_file.write(final_html)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     os.makedirs(dest_dir_path, exist_ok=True)
     
     for file in os.listdir(dir_path_content):
@@ -40,8 +42,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         
         if file.endswith(".md"):
             dest_file_path = os.path.join(dest_dir_path, file.replace(".md", ".html"))
-            generate_page(full_path, template_path, dest_file_path)
+            generate_page(full_path, template_path, dest_file_path, basepath)
         
         elif os.path.isdir(full_path):
             dest_subdir_path = os.path.join(dest_dir_path, file)
-            generate_pages_recursive(full_path, template_path, dest_subdir_path)
+            generate_pages_recursive(full_path, template_path, dest_subdir_path, basepath)
