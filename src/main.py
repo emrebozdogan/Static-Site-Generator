187d0c6e151d90331778db0577df867b1f170372
emrebozdogan <emrbzdgn@icloud.com>
2025-08-22 14:46:32 +0300

    Update main.py - switch from generate_page to generate_pages_recursive for full site generation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from textnode import *
 from copy_static import *
 from gencontent import *
+import sys
+
 
 def main():
-    copy_static("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
-
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    copy_static("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+    
 if __name__ == "__main__":
     main()
